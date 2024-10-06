3ef78a9747d7d8321469e26db128b92dfaa40ca2
Amazad3245 <amazadweb3@gmail.com>
2024-10-06 17:56:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function shortcut(s1, s2) {
   // your code here
-	if (s1.length > 0 && s2.length > 0) {
-    return s1[0].toUpperCase() + s2[0].toUpperCase();
-  } else {
-    return '';
-  }
+	 if (s1.length > 0 && s2.length > 0) {
+      return s1[0] + s2[0];
+    } else if(s1 == 0 ){
+        return s2[0];
+    }else if(s2 == 0){
+        return s1[0];
+    }else{
+        return '';
+    }
 }
 
 // Do not change the code below.
